1880d9371d4d1a47737aa6e089026f4dc3cc16bb
JinseoKim <eunoia@handong.ac.kr>
2023-12-18 05:49:42 +0900

    firebase project ID 수정

--- fbase.js
+++ fbase.js
@@ -27,7 +27,7 @@ import {
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: "drop-the-ball-9bf63",
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
