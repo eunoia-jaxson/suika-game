3fb07fec01f977cfc041a99b9624ab79aae3cd07
JinseoKim <eunoia@handong.ac.kr>
2023-12-20 01:18:05 +0900

    환경변수 설정

--- fbase.js
+++ fbase.js
@@ -25,13 +25,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAr6RL9VXmrwRF8ZuXRh9tChi_xC66GN1I",
-  authDomain: "drop-the-ball-9bf63.firebaseapp.com",
-  projectId: "drop-the-ball-9bf63",
-  storageBucket: "drop-the-ball-9bf63.appspot.com",
-  messagingSenderId: "766229072214",
-  appId: "1:766229072214:web:c359b563462fa60c4caef7",
-  measurementId: "G-CE655T4BQJ",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
